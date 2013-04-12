798d1d8d1f5b79aab34ff0df5075243134190677
Adrian Spanu <aspanu@stanford.edu>
2013-04-12 15:20:56 -0700

    Updates to test_mrInit to correctly test mrInit that no longer saves out functional data.

--- bold/test_mrInit.m
+++ bold/test_mrInit.m
@@ -88,9 +88,9 @@ for scanNumber = 1:length(params.functionals)
     
     val = niftiGet(epi_nii{scanNumber},'Dim');
     % Number of TRs:
-    assertEqual(func(scanNumber).nFrames, val(end) - params.keepFrames(scanNumber,1));
+    assertEqual(func(scanNumber).nFrames - func(scanNumber).keepFrames(scanNumber,1), val(end) - params.keepFrames(scanNumber,1));
     % also  in dataTYPES:
-    assertEqual(dt.scanParams(scanNumber).nFrames, val(end)-params.keepFrames(scanNumber,1));
+    assertEqual(dt.scanParams(scanNumber).nFrames - dt.scanParams(scanNumber).keepFrames(scanNumber,1), val(end)-params.keepFrames(scanNumber,1));
     
     % Inplane dimensions:
     assertEqual(func(scanNumber).fullSize, niftiGet(epi_nii{scanNumber},'Slice Dims'));
